ec5e1f362baf9ffb3d78fe56ef2b08ae7a87ea87
NAQVI <nomanalam7214@gmail.com>
2019-08-16 17:41:22 +0200

    Updates

---
+++ precache-manifest.6fe28d4fb906212d2d5c379dcab2f28f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10fdc4c36dced1fa5e1da20bd583eb7",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1f799b79cb3319a00992",
+    "url": "./static/css/main.157f8779.chunk.css"
+  },
+  {
+    "revision": "40eebbbbbddf37bcb4a2",
+    "url": "./static/js/2.8538be6a.chunk.js"
+  },
+  {
+    "revision": "1f799b79cb3319a00992",
+    "url": "./static/js/main.72d03a34.chunk.js"
+  },
+  {
+    "revision": "af109f24ffc7cd5edeee",
+    "url": "./static/js/runtime~main.33f8bc02.js"
+  }
+]);
